a781d35a78c600d7780e420c673b9bd02d7fd95d
SEH00N <06psh0501@gmail.com>
2024-01-06 18:13:43 +0900

    unit health

---
+++ ProjectAppjam/Assets/01. Scripts/Unit/Component/UnitHealth.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnitHealth : UnitComponent, IDamageable
+{
+    private float currentHP = 0f;
+
+    public UnityEvent<GameObject, Vector3> OnDamagedEvent;
+    public UnityEvent<GameObject> OnDeadEvent;
+
+    public override void Init(UnitController controller)
+    {
+        base.Init(controller);
+        currentHP = controller.UnitData.MaxHP;
+    }
+
+    public void OnDamaged(int damage = 0, GameObject performer = null, Vector3 point = default)
+    {
+        if(controller.IsDead)
+            return;
+
+        currentHP -= damage;
+        OnDamagedEvent?.Invoke(performer, point);
+
+        if (currentHP <= 0f)
+        {
+            OnDeadEvent?.Invoke(performer);
+            controller.IsDead = true;
+        }
+    }
+}
