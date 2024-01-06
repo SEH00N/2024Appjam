87875078a39b2c6d1b1fbeb8b90fc2693be71109
SEH00N <06psh0501@gmail.com>
2024-01-06 17:09:26 +0900

    unit movement

---
+++ ProjectAppjam/Assets/01. Scripts/Unit/Component/UnitMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class UnitMovement : UnitComponent
+{
+    private Quaternion targetRotation;
+	private Vector3 targetPosition;
+    private Vector3 moveDir;
+
+    private float moveSpeed;
+    private float rotateSpeed;
+
+    private bool isArrived = false;
+
+    public override void Init(UnitController controller)
+    {
+        base.Init(controller);
+
+        moveSpeed = controller.UnitData.MoveSpeed;
+        rotateSpeed = controller.UnitData.RotateSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        if(isArrived)
+            return;
+        
+        Rotate();
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 moveVector = moveDir * moveSpeed * Time.fixedDeltaTime;
+        transform.position += moveVector;
+    }
+
+    private void Rotate()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotateSpeed);
+    }
+
+    public void SetTargetPosition(Vector3 pos)
+    {
+        targetPosition = pos;
+
+        Vector3 dir = targetPosition - transform.position;
+        moveDir = dir.normalized;
+
+        targetRotation = Quaternion.LookRotation(moveDir);
+        isArrived = (dir.sqrMagnitude < 0.1f);
+    }
+
+    public void MoveImmediately(Vector3 pos)
+    {
+        SetTargetPosition(pos);
+        transform.position = pos;
+    }
+
+    public void StopImmediately()
+    {
+        MoveImmediately(transform.position);
+    }
+}
