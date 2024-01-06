714baa20f4ccda80bf1113cc2a121430abf757d0
SEH00N <06psh0501@gmail.com>
2024-01-06 22:34:43 +0900

    asd

--- ProjectAppjam/Assets/01. Scripts/Core/CameraManager.cs
+++ ProjectAppjam/Assets/01. Scripts/Core/CameraManager.cs
@@ -1,6 +1,68 @@
+using System.Collections;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-	
+	private static CameraManager instance;
+    public static CameraManager Instance {
+        get {
+            if(instance==null)
+                instance = FindObjectOfType<CameraManager>();
+            return instance;
+        }
+    }
+
+    private CinemachineVirtualCamera mainVCam;
+    public CinemachineVirtualCamera MainVCam {
+        get {
+            if(mainVCam == null)
+                mainVCam = GameObject.Find("MainVCam").GetComponent<CinemachineVirtualCamera>();
+            return mainVCam;
+        }
+    }
+
+    private Camera mainCam;
+    public Camera MainCam {
+        get {
+            if (mainCam == null)
+                mainCam = Camera.main;
+            return mainCam;
+        }
+    }
+
+    private CinemachineBasicMultiChannelPerlin perlin = null;
+    private bool onShake = false;
+    
+    private void Awake()
+    {
+        perlin = mainVCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    private void Start()
+    {
+        perlin.m_AmplitudeGain = 0f;
+        perlin.m_FrequencyGain = 0f;
+    }
+
+    public void ShakeCam(float duration, float power, float frequency)
+    {
+        if(onShake) return;
+
+        onShake = true;
+        perlin.m_AmplitudeGain = power;
+        perlin.m_FrequencyGain = frequency;
+
+        StartCoroutine(PerlinResetCoroutine(duration));
+    }
+
+    private IEnumerator PerlinResetCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        perlin.m_AmplitudeGain = 0f;
+        perlin.m_FrequencyGain = 0f;
+
+        onShake = false;
+    }
 }
---
+++ ProjectAppjam/Assets/01. Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] float maxHP = 100;
+    private float currentHP = 0f;
+
+    public UnityEvent<GameObject, Vector3> OnDamagedEvent;
+    public UnityEvent<GameObject> OnDeadEvent;
+
+    public bool IsDead;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+    }
+
+    public void OnDamaged(int damage = 0, GameObject performer = null, Vector3 point = default)
+    {
+        if(IsDead)
+            return;
+
+        currentHP -= damage;
+        OnDamagedEvent?.Invoke(performer, point);
+
+        if (currentHP <= 0f)
+        {
+            OnDeadEvent?.Invoke(performer);
+            OnDie(performer);
+            IsDead = true;
+        }
+    }
+
+    private void OnDie(GameObject performer)
+    {
+    }
+}
